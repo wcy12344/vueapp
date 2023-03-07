6824d78d31f70f034ffaeaff12f33650f2a40dbc
1008611a <18532224709@163.com>
2023-03-07 20:29:22 +0800

    chore: init

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-don't let 💩 slip into your code base!
+目前想采用微前端(暂时想采用无界)+一些前沿技术，或者能够简化开发的插件做的一个vue项目
 
 vite + vue3 + ts + eslint + vueuse
 
-husky + 
+husky + lint-staged + commitizen 规范git提交
+  don't let 💩 slip into your code base!
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,36 @@
 import { URL, fileURLToPath } from 'node:url'
-
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dts: true,
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        'vue/macros',
+        '@vueuse/head',
+        '@vueuse/core',
+      ],
+      dts: 'src/auto-imports.d.ts',
+      dirs: [
+        'src/composables',
+        'src/stores',
+      ],
+      vueTemplate: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
