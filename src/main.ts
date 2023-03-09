10871f54408da38489f6319d4d83d0cf72cc1f74
1008611a <18532224709@163.com>
2023-03-09 11:55:55 +0800

    chore: init

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { Button, message } from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import 'normalize.css'
 import '@/assets/main.css'
+import 'ant-design-vue/dist/antd.css'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(Button)
 app.mount('#app')
+app.config.globalProperties.$message = message
